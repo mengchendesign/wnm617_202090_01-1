c703f07072951ab2b7c40bc46838299703bc6290
mengchendesign <70695884+mengchendesign@users.noreply.github.com>
2020-11-05 12:50:56 -0800

    Module 9

--- chen.meng/js/parts.js
+++ chen.meng/js/parts.js
@@ -18,9 +18,13 @@ const makeUserProfile = templater(o=>`
    <div class="user-profile-image">
       <img src="${o.img}" alt="">
    </div>
+   <div class="user-form">
    <h2>${o.name}</h2>
    <h3>@${o.username}</h3>
+   <div class="user-button">
    <div><a href="#user-settings-page">Settings</a></div>
+   </div>
+   </div>
    `);
 
 const makeAnimalProfile = templater(o=>`
