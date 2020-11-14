3ed03876a02660c66266168e716fbc51bf51c75d
mengchendesign <70695884+mengchendesign@users.noreply.github.com>
2020-11-14 12:31:52 -0800

    Module 10

---
+++ chen.meng/app.js
@@ -0,0 +1,67 @@
+
+
+
+$(()=>{
+
+
+
+
+   checkUserId();
+
+   // event delegation
+   $(document)
+
+   .on("pagecontainerbeforeshow",function(e,ui) {
+      // console.log(ui.toPage[0].id)
+
+      // Page Routing 
+      switch(ui.toPage[0].id) {
+         case "recent-page": RecentPage(); break;
+         case "list-page": ListPage(); break;
+         case "user-profile-page": UserProfilePage(); break;
+         case "animal-profile-page": AnimalProfilePage(); break;
+      }
+   })
+
+
+
+   // FORM SUBMITS
+
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+
+
+
+
+   // ANCHOR CLICKS
+
+   .on("click",'.js-logout',function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+   .on("click",'.js-animal-jump',function(e){
+      sessionStorage.animalId = $(this).data("id");
+      $.mobile.navigate('#animal-profile-page');
+   })
+
+
+
+
+   .on('click','[data-activate]',function(e){
+      let target = $(this).data('activate');
+      $(target).addClass("active")
+   })
+   .on('click','[data-deactivate]',function(e){
+      let target = $(this).data('deactivate');
+      $(target).removeClass("active")
+   })
+   .on('click','[data-toggle]',function(e){
+      let target = $(this).data('toggle');
+      $(target).toggleClass("active")
+   })
+
+})
